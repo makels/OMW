b48800a7d7d376955a8d0f8461c4c788c5652c80
Serhii Shulha <sv_shulga@ukr.net>
2017-11-11 11:01:38 +0200

    Dump

---
+++ omw.sql
@@ -0,0 +1,494 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 11 2017 г., 11:00
+-- Версия сервера: 10.0.32-MariaDB-0+deb8u1
+-- Версия PHP: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `zergsolutions`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `parent_id`, `name`, `description`) VALUES
+(1, 0, 'Hero Pro', ''),
+(2, 0, 'Hero', ''),
+(3, 0, 'Arena', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `rest` double(10,2) NOT NULL,
+  `ball` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `clients_reviews`
+--
+
+CREATE TABLE `clients_reviews` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `washer_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `review` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `active` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=MyISAM AVG_ROW_LENGTH=8192 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'Системный администратор', 'Позволяет управлять системными настройками, управлять пользователями, группами пользователей');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `main_image` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `date_time` datetime NOT NULL,
+  `up_slider` varchar(255) NOT NULL,
+  `down_slider` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `options`
+--
+
+CREATE TABLE `options` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `excel_column` int(11) NOT NULL,
+  `show` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `washer_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `service` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `date_time` datetime NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 - новый, 1 - принят, 2 - в работе, 3 - завершен',
+  `create_order` datetime NOT NULL,
+  `flyer_number` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `model` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products_category`
+--
+
+CREATE TABLE `products_category` (
+  `category_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products_options`
+--
+
+CREATE TABLE `products_options` (
+  `product_id` int(11) NOT NULL,
+  `option_id` int(11) NOT NULL,
+  `string_value` varchar(255) NOT NULL,
+  `int_value` int(11) NOT NULL,
+  `double_value` double NOT NULL,
+  `bool_value` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=cp1251;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `service` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `display_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `su` tinyint(1) DEFAULT '0'
+) ENGINE=MyISAM AVG_ROW_LENGTH=72 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `display_name`, `pass`, `email`, `su`) VALUES
+(1, 'admin', 'Администратор', '21232f297a57a5a743894a0e4a801fc3', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_groups`
+--
+
+CREATE TABLE `users_groups` (
+  `group_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=MyISAM AVG_ROW_LENGTH=8192 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users_groups`
+--
+
+INSERT INTO `users_groups` (`group_id`, `user_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_groups_permissions`
+--
+
+CREATE TABLE `users_groups_permissions` (
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=MyISAM AVG_ROW_LENGTH=4096 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users_groups_permissions`
+--
+
+INSERT INTO `users_groups_permissions` (`group_id`, `permission_id`) VALUES
+(1, 2),
+(1, 4),
+(1, 3),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+(1, 1),
+(1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_permissions`
+--
+
+CREATE TABLE `users_permissions` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `alias` varchar(255) NOT NULL
+) ENGINE=MyISAM AVG_ROW_LENGTH=5461 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users_permissions`
+--
+
+INSERT INTO `users_permissions` (`id`, `name`, `alias`) VALUES
+(1, 'Управление пользователями', 'users'),
+(2, 'Импорт Excel', 'excel'),
+(3, 'Система', 'system'),
+(4, 'Новости', 'news'),
+(5, 'Управление товарами', 'products'),
+(6, 'Управление заявками', 'orders'),
+(7, 'Управление клиентами', 'clients'),
+(8, 'Управление мойщиками', 'washers'),
+(9, 'Управление отзывами', 'reviews');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `washers`
+--
+
+CREATE TABLE `washers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `transport` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `washers_reviews`
+--
+
+CREATE TABLE `washers_reviews` (
+  `id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `washer_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `rating` int(11) NOT NULL,
+  `review` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `washers_reviews`
+--
+
+INSERT INTO `washers_reviews` (`id`, `client_id`, `washer_id`, `date`, `rating`, `review`, `active`) VALUES
+(2, 6, 10, '2017-09-23 15:43:42', 4, 'Приехал вовремя. Сделал все четко и быстро. Спасибо.', 1),
+(3, 6, 10, '2017-09-23 15:46:53', 5, 'Все круто !!! Рекомендую !', 1),
+(4, 15, 19, '2017-10-27 15:23:58', 4, 'Все норм', 0);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `clients_reviews`
+--
+ALTER TABLE `clients_reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Индексы таблицы `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Индексы таблицы `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usr` (`name`);
+
+--
+-- Индексы таблицы `users_permissions`
+--
+ALTER TABLE `users_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Индексы таблицы `washers`
+--
+ALTER TABLE `washers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `washers_reviews`
+--
+ALTER TABLE `washers_reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT для таблицы `clients_reviews`
+--
+ALTER TABLE `clients_reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT для таблицы `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT для таблицы `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+--
+-- AUTO_INCREMENT для таблицы `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+--
+-- AUTO_INCREMENT для таблицы `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT для таблицы `users_permissions`
+--
+ALTER TABLE `users_permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT для таблицы `washers`
+--
+ALTER TABLE `washers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+--
+-- AUTO_INCREMENT для таблицы `washers_reviews`
+--
+ALTER TABLE `washers_reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
